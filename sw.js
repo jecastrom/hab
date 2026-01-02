ec5ac4e7f0753e60b30b1ff9eb31e34b9d38e6f3
Jorge <jcastro.de@outlook.com>
2026-01-02 21:02:05 +0100

    commit

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
-const CACHE_NAME = 'melder-cache-v8';
+const CACHE_NAME = 'melder-cache-v9';
+const LIB_URL = 'https://unpkg.com/@github/webauthn-json@2.1.1/dist/browser-global.js';
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['/', '/index.html', '/admin.html', '/login.html', '/manifest.json', '/auth-guard.js', LIB_URL]);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
-      keys.map((key) => caches.delete(key))
+      keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
     ))
   );
   self.clients.claim();
@@ -19,9 +25,7 @@ self.addEventListener('fetch', (event) => {
       .then((response) => {
         if (response.status === 200) {
           const resClone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, resClone);
-          });
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
         }
         return response;
       })

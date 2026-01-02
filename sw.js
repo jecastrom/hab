c3f6f3eda06ff3907dff0135729215d5a0c332b3
Jorge <jcastro.de@outlook.com>
2026-01-02 02:13:17 +0100

    commit

--- sw.js
+++ sw.js
@@ -1,62 +1,42 @@
-const CACHE_NAME = 'melder-cache-v1';
-const DATA_CACHE_NAME = 'melder-data-v1';
+const CACHE_NAME = 'melder-cache-v3';
 
-// Files to cache immediately (App Shell)
+// Files to cache for offline use
 const urlsToCache = [
   '/',
   '/index.html',
-  '/admin.html'
+  '/admin.html',
+  '/manifest.json'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
   self.skipWaiting();
 });
 
-// Activate Event
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => caches.delete(key)) // Clear old caches on version change
+    ))
   );
   self.clients.claim();
 });
 
-// Fetch Event
 self.addEventListener('fetch', (event) => {
-  // Logic for API Data (get-objects and get-data)
-  if (event.request.url.includes('/api/')) {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            // If network is OK, clone response and save to cache
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(() => {
-            // If network fails (Offline), try the cache
-            return cache.match(event.request.url);
-          });
-      })
-    );
-    return;
-  }
-
-  // Logic for App Shell (HTML, CSS, JS)
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        // If network is successful, update the cache
+        if (response.status === 200) {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, resClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        // If network fails (offline), return from cache
+        return caches.match(event.request);
+      })
   );
 });
